2e23c980e3d709fd24ce6921f2cfa591f46b6b67
Alexander Kern <alexander.t.kern@gmail.com>
2025-04-17 15:11:26 -0500

    Window Init fix and close functions

--- window.py
+++ window.py
@@ -2,14 +2,21 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window(): 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title = "Maze Solver AK"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__root = Tk()
+        self.__root.title = "Maze Solver AK"
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self): 
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self): 
+        self.__running = True
+        while self.__running: 
+            self.redraw()
+    
+    def close(self): 
+        self.__running = False
