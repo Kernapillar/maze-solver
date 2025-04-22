eb939c98d0e44eab8e641318d5d600f01c84d88e
Alexander Kern <alexander.t.kern@gmail.com>
2025-04-22 10:53:13 -0500

    maze break walls testing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from maze import Maze
 def main(): 
     win = Window(800, 600)
 
-    maze = Maze(100, 100, 4, 4, 50, 50, win)
-    
+    maze = Maze(100, 100, 10, 10, 30, 30, win)
+
 
     win.wait_for_close()
 
